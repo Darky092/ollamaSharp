0407bfbbfb235da9a966dc3986624dcc25483c35
Darky092 <112990580+Darky092@users.noreply.github.com>
2025-02-13 17:29:55 +0300

    Add files via upload
    внимательно прочитайте readme.md для корректной работы приложения

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using OllamaSharp;
+
+namespace wpfOllama
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void askQuestion_Click(object sender, RoutedEventArgs e)
+        {
+            await InitModel(question.Text);
+        }
+
+        private async Task InitModel(string message)
+        {
+            try
+            {
+                var uri = new Uri("http://localhost:11434");
+                var ollama = new OllamaApiClient(uri);
+                ollama.SelectedModel = "llama2";
+
+                var chat = new Chat(ollama);
+                string systemMessage = "Ты специалист наводного транспорта всяких лодок и судов ";
+
+                if (string.IsNullOrWhiteSpace(message))
+                {
+                    MessageBox.Show("Введите вопрос!");
+                    return;
+                }
+
+                List<string> answer = new List<string>();
+
+                await foreach (var answerToken in chat.SendAsync($"{systemMessage} {message}"))
+                {
+                    answer.Add(answerToken.ToString());
+                    
+                    Dispatcher.Invoke(() => Answer.Text = string.Join("", answer.ToArray()));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private async void askQuestion_Click_1(object sender, RoutedEventArgs e)
+        {
+            await InitModel(question.Text);
+
+        }
+    }
+}
